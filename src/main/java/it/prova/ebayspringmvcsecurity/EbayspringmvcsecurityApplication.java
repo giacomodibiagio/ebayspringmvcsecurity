96faa38a19166a9ff563038c1da933ba20fa2d48
giacomodibiagio <dibiagio.giacomo@gmail.com>
2021-05-13 17:10:17 +0200

    first commit

---
+++ src/main/java/it/prova/ebayspringmvcsecurity/EbayspringmvcsecurityApplication.java
@@ -0,0 +1,13 @@
+package it.prova.ebayspringmvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayspringmvcsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayspringmvcsecurityApplication.class, args);
+	}
+
+}
